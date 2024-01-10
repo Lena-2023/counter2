f0a23e75f9ef55d173d08a505b8b97ec2576b616
Lena-2023 <152642024+Lena-2023@users.noreply.github.com>
2024-01-10 20:22:07 +0300

    Add files via upload

--- tcounter.v
+++ tcounter.v
@@ -1,9 +1,10 @@
-                    module testbench();
+module testbench();
 
 reg clk;
-reg [7:0] get;
+reg reset;
+reg [7:0] qa;
 
-//testbench testbench(.clk(clk), .get(get));
+testbench testbench1(.clk(clk), .get(get));
 
 initial
 begin
@@ -11,77 +12,29 @@ begin
     $dumpvars(0,testbench);
 
     $display("starting testbench!!!!");
-	
-	clk <= 0; 
-	repeat (3000) begin
-		#10;
-		clk <= 1;
-		#10;
-		clk <= 0; 
-	end
-	
+    
+    $monitor(reset,, clk,, qa);
+  
+        clk <= 0; 
+        repeat (3000) 
+            
+        begin
+                #10;
+                clk <= 1;
+                #10;
+                clk <= 0; 
+        end
+
+
     $display("finished OK!");
     //$finish;
 end
 
-
-//initial
-//begin
-	
-initial begin 
-clk=0;
-forever #5 clk=~clk;
+initial
+begin
+  reset <= 1;
+  #100;
+  reset <= 0;
 end
 
-initial begin
-get = 8'b0; 
-#20;
-get = get+1;
-
-
-end
-
-
-	
-	/*get <= 0;
-	#100;
-	get <= 1;
-	#25;
-	get <= 0;
-
-	#100;
-	get <= 1;
-	#25;
-	get <= 0;
-
-	#100;
-	get <= 1;
-	#25;
-	get <= 0;
-
-	#100;
-	get <= 1;
-	#25;
-	get <= 0;
-
-	#100;
-	get <= 1;
-	#25;
-	get <= 0;
-
-	#100;
-	get <= 1;
-	#25;
-	get <= 0;
-
-	#100;
-	get <= 1;
-	#25;
-	get <= 0;
-	*/
-	
-//end 
-             //убрать коомент
-
-	
 endmodule
